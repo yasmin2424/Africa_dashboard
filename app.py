77525e1c4e0e46169f8d144c880eff1727fb41e1
Forsythia-olive <forgiveagbesi7478@gmail.com>
2025-05-24 22:33:01 -0700

    Update literacy chart

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import dcc, html
 import dash_bootstrap_components as dbc
 from youth_chart import get_youth_population_chart
 from unemployment_chart import get_unemployment_chart
+from literacy import get_literacy_chart
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
@@ -43,6 +44,7 @@ app.layout = dbc.Container([
 
         dcc.Tab(label='3. Shared Challenges', children=[
             html.Br(),
+            dcc.Graph(figure=get_literacy_chart()),
             dcc.Graph(figure=get_unemployment_chart())
         ]),
         dcc.Tab(label='4. Our Vision: African Youth Exchange', children=[
--- literacy.py
+++ literacy.py
@@ -6,9 +6,9 @@ import os
 def process_data(path):
     # Load unemployment data from the assets folder
     employ_df = pd.read_csv(path, skiprows=3)
-    countries_of_interest = ["Ethiopia", "Ghana", "Kenya", "Nigeria", "Sub-Saharan Africa", "Uganda", "World"]
+    countries_of_interest = ["Sub-Saharan Africa", "World"]
 
-    columns_of_interest = ['Country Name'] + [str(year) for year in range(2011, 2025)]
+    columns_of_interest = ['Country Name'] + [str(year) for year in range(2011, 2024)]
     df_selected = employ_df[columns_of_interest]
 
     df_filtered = df_selected[df_selected['Country Name'].isin(countries_of_interest)]
@@ -18,14 +18,14 @@ def process_data(path):
     
     return df_filtered
 
-def get_unemployment_chart():
+def get_literacy_chart():
 
-    df = process_data("assets/unemployment.csv")
+    df = process_data("assets/Literacy_levels.csv")
     df_long = df.melt(id_vars='Country Name', var_name='Year', value_name='Value')
     df_long['Year'] = df_long['Year'].astype(int)
 
     # Create the plot
     fig = px.line(df_long, x='Year', y='Value', color='Country Name',
-                title='Unemployment Trends (2011–2024)',
+                title='Literacy Trend  (2011–2023)',
                 labels={'Value': 'Value', 'Year': 'Year'})
     return fig
